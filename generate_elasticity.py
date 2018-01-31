a2fec6a4629bd53b0f468b58b89b34b049efb806
andybond13 <andybond13@yahoo.com>
2018-01-30 22:06:02 -0800

    Update generate_elasticity to access elasticity for writing.

--- generate_elasticity.py
+++ generate_elasticity.py
@@ -14,7 +14,6 @@ import sys
 def read_file(filename):
     base = os.path.splitext(filename)[0]
     format = base[-2:]
-    print format
     assert(format in ['F2','F4'])
     data = np.load(filename)
     return data, format
@@ -30,7 +29,7 @@ def write_elasticity(filename, elasticity, order):
         key = 'ijkl'
     elif order == 6:
         key = 'ijklmn'
-    C_ijkl = '1111 1112 1113 1121 1122 1123 1131 1132 1133   1112 1212 1213 1221 1222 1223 1231 1232 1233    1113 1213 1313 1321 1322 1323 1331 1332 1333     1121 1221 1321 2121 2122 2123 2131 2132 2133     1122 1222 1322 2122 2222 2223 2231 2232 2233     1123 1223 1323 2123 2223 2323 2331 2332 2333     1131 1231 1331 2131 2231 2331 3131 3132 3133     1132 1232 1332 2132 2232 2332 3132 3232 3233     1133 1233 1333 2133 2233 2333 3133 3233 3333'
+#    C_ijkl = '1111 1112 1113 1121 1122 1123 1131 1132 1133   1112 1212 1213 1221 1222 1223 1231 1232 1233    1113 1213 1313 1321 1322 1323 1331 1332 1333     1121 1221 1321 2121 2122 2123 2131 2132 2133     1122 1222 1322 2122 2222 2223 2231 2232 2233     1123 1223 1323 2123 2223 2323 2331 2332 2333     1131 1231 1331 2131 2231 2331 3131 3132 3133     1132 1232 1332 2132 2232 2332 3132 3232 3233     1133 1233 1333 2133 2233 2333 3133 3233 3333'
     dim = 3
     entries = []
 
@@ -58,22 +57,18 @@ def write_elasticity(filename, elasticity, order):
 
     data = []
     for entry in entries:
-        if (order == 2):
-            data.append( elasticity[entry[0], entry[1]] )
-        elif (order == 4):
-            print "entry = ",entry
-            print "len(elast) = ",len(elasticity)
-            print "shape = ",elasticity.shape
-            data.append( elasticity[entry] )
-        else:
-            assert(1 == 0)
+        entryList = []
+        for letter in entry:
+            entryList.append(int(letter))
+        entryList = tuple(entryList)
+        data.append( elasticity[entryList] )
     print data
 
     return    
 
 def calc_rahmoun(data, order):
     dim = 3
-    shape = (dim,3)*order
+    shape = (dim,)*order
 
     elasticity = np.zeros( shape )
     return elasticity
